bd2b03c835055776d739dd064d80c5dd5ec9af40
Dzulhelmy <trial.dzulhelmy@gmail.com>
2025-11-30 01:37:05 +0800

    Comment out static export configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: "export", // Enable static export for GitHub Pages
+	// output: "export",
 	images: {
 		// unoptimized: true,
 		remotePatterns: [
